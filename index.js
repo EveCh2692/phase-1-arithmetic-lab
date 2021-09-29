0e32e1725dc9058af9832cdfe3ed98a649f9ce4f
Evelyn <evelyn.chavez92@yahoo.com>
2021-09-28 22:51:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let num1 = 31
  let num2 = 2
+const multiply = num1*num2
+const random = Math.ceil( Math.random() * 10 ) + 2;
+let num3 = 22
+let num4 = 6
+const mod = num3 % num4
+let num5 = [2,4,6,8,10,12,14,16,18]
+const max = Math.max(num3)
 
-function multiply(){
-    
- 
- return num1*num2
  
  
-}
